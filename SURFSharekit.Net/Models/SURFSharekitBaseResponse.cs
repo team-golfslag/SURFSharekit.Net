3b3108665c634f262721d4457b2773f62c4caf5b
Tijmen Vis <tijmenpvis@gmail.com>
2025-05-21 11:39:01 +0200

    docs: updated docs on Attributes object in baseresponse

--- SURFSharekit.Net/Models/SURFSharekitBaseResponse.cs
+++ SURFSharekit.Net/Models/SURFSharekitBaseResponse.cs
@@ -4,14 +4,22 @@
 // © Copyright Utrecht University (Department of Information and Computing Sciences)
 
 using System.Text.Json.Serialization;
+using SURFSharekit.Net.Models.RepoItem;
+using SURFSharekit.Net.Models.Webhooks;
 
 namespace SURFSharekit.Net.Models;
 
 public class SURFSharekitBaseResponse
 {
+    //type is object, because it could be
+    /// <summary>
+    /// Type is object, because it could either be a <see cref="SURFSharekitWebhookCreate"/>,
+    /// <see cref="SURFSharekitRepoItem"/> (both <see cref="SURFSharekitAttributes"/>) or
+    /// <see cref="SURFSharekitWebhookDelete"/> (an empty list)
+    /// </summary>
     [JsonPropertyName("attributes")]
     public object? Attributes { get; set; }
-    
+
     [JsonPropertyName("type")]
     public string? Type { get; set; }
 
